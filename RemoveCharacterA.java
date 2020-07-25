01108b5332fbd5734c588b1932dd280df878f985
Sanjatha1999 <60172120+Sanjatha1999@users.noreply.github.com>
2020-07-25 18:16:48 +0530

    Create RemoveCharacterA.java

---
+++ RemoveCharacterA.java
@@ -0,0 +1,10 @@
+  
+package com.epam.design_patterns.tddjunit;
+
+public class RemoveCharacterA {
+	public String remove(String string) {
+		int length = string.length();
+		return string.substring(0,Math.min(length, 2)).replace("A", "") +
+			   string.substring(Math.min(length, 2),length);
+	}
+}
